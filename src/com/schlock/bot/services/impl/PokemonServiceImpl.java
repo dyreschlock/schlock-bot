cad914f4af9e2bafbf6569a9ed7b767dce22d8db
Jim Hendricks <dyreschlock@gmail.com>
2021-02-09 12:32:17 +0900

    given a range of pokemon, returns one random // #4

--- src/com/schlock/bot/services/impl/PokemonServiceImpl.java
+++ src/com/schlock/bot/services/impl/PokemonServiceImpl.java
@@ -9,10 +9,7 @@ import org.jsoup.nodes.Element;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public class PokemonServiceImpl implements PokemonService
 {
@@ -22,6 +19,9 @@ public class PokemonServiceImpl implements PokemonService
 
     protected static final String POKEMON_RETURN_FORMAT = "No. %s %s";
     protected static final String POKEMON_RETURN_NULL = "What?";
+
+    private static final Integer FIRST_POKEMON = 1;
+    private static final Integer LAST_POKEMON = 898;
 
     private final DeploymentContext context;
 
@@ -59,23 +59,77 @@ public class PokemonServiceImpl implements PokemonService
     private Pokemon processPokemon(String in)
     {
         String commandText = in.substring(POKEMON_COMMAND.length());
-        commandText = commandText.trim();
-        commandText = commandText.replace(" ", "");
+        commandText = cleanText(commandText);
 
-        //if starts with -
+        if (commandText.startsWith("-"))
+        {
+            String pokemon = commandText.split("-")[1];
 
-        //if ends with -
+            Pokemon start = pokemonByNumber.get(FIRST_POKEMON);
+            Pokemon end = getPokemonByNameNumber(pokemon);
 
-        //if contains -
+            return getPokemonInRange(start, end);
+        }
+        if (commandText.endsWith("-"))
+        {
+            String pokemon = commandText.split("-")[0];
 
-        //if is number
-        Integer number = getNumber(commandText);
+            Pokemon start = getPokemonByNameNumber(pokemon);
+            Pokemon end = pokemonByNumber.get(LAST_POKEMON);
+
+            return getPokemonInRange(start, end);
+        }
+        if (commandText.contains("-"))
+        {
+            String pokemonStart = commandText.split("-")[0];
+            String pokemonEnd = commandText.split("-")[1];
+
+            Pokemon start = getPokemonByNameNumber(pokemonStart);
+            Pokemon end = getPokemonByNameNumber(pokemonEnd);
+
+            return getPokemonInRange(start, end);
+        }
+        return getPokemonByNameNumber(commandText);
+    }
+
+    private Pokemon getPokemonByNameNumber(String commandText)
+    {
+        String text = cleanText(commandText);
+
+        Integer number = getNumber(text);
         if(number != null)
         {
             return pokemonByNumber.get(number);
         }
-        return pokemonByName.get(commandText);
+        return pokemonByName.get(text);
     }
+
+    private Pokemon getPokemonInRange(Pokemon start, Pokemon finish)
+    {
+        if (start == null || finish == null)
+        {
+            return null;
+        }
+
+        Integer startNumber = start.getNumber();
+        Integer finishNumber = finish.getNumber();
+        if (startNumber > finishNumber)
+        {
+            startNumber = finish.getNumber();
+            finishNumber = start.getNumber();
+        }
+        if (startNumber == finishNumber)
+        {
+            return start;
+        }
+
+        int total = finishNumber - startNumber +1;
+        int random = new Random().nextInt(total);
+        random = random + startNumber;
+
+        return pokemonByNumber.get(random);
+    }
+
 
     private Integer getNumber(String in)
     {
@@ -88,6 +142,12 @@ public class PokemonServiceImpl implements PokemonService
         {
         }
         return null;
+    }
+
+    private String cleanText(String text)
+    {
+        String newText = text.trim().toLowerCase().replace(" ", "");
+        return newText;
     }
 
 
--- test/com/schlock/bot/services/impl/PokemonServiceImplTest.java
+++ test/com/schlock/bot/services/impl/PokemonServiceImplTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,7 +29,7 @@ class PokemonServiceImplTest
         for (String[] data : testData)
         {
             String test = impl.process(data[0]);
-            assertEquals(test, data[1], String.format("Incoming data [%s] // Outgoing data [%s]", data[0], test));
+            assertEquals(test, data[1]);
         }
     }
 
@@ -46,9 +48,66 @@ class PokemonServiceImplTest
         for (String[] data : testData)
         {
             String test = impl.process(data[0]);
-            assertEquals(test, data[1], String.format("Incoming data [%s] // Outgoing data [%s]", data[0], test));
+            assertEquals(test, data[1]);
         }
     }
+
+    @Test
+    public void testPokemonRangeSyntaxOnePokemon()
+    {
+        List<String[]> testData = new ArrayList<>();
+        testData.add(new String[]{"!pokemon bulbasaur-bulbasaur", "No. 1 Bulbasaur"});
+        testData.add(new String[]{"!pokemon 1-1", "No. 1 Bulbasaur"});
+        testData.add(new String[]{"!pokemon 1-bulbasaur", "No. 1 Bulbasaur"});
+        testData.add(new String[]{"!pokemon bulbasaur-1", "No. 1 Bulbasaur"});
+
+        for (String[] data : testData)
+        {
+            String text = impl.process(data[0]);
+            assertEquals(text, data[1]);
+        }
+    }
+
+    @Test
+    public void testPokemonRangeTwoPokemon()
+    {
+        List<String[]> testData = new ArrayList<>();
+        testData.add(new String[]{"!pokemon 1-2", "No. 1 Bulbasaur", "No. 2 Ivysaur"});
+        testData.add(new String[]{"!pokemon 2-1", "No. 1 Bulbasaur", "No. 2 Ivysaur"});
+
+        testData.add(new String[]{"!pokemon -3", "No. 1 Bulbasaur", "No. 2 Ivysaur", "No. 3 Venusaur"});
+        testData.add(new String[]{"!pokemon -2", "No. 1 Bulbasaur", "No. 2 Ivysaur"});
+        testData.add(new String[]{"!pokemon -1", "No. 1 Bulbasaur"});
+        testData.add(new String[]{"!pokemon -0", PokemonServiceImpl.POKEMON_RETURN_NULL});
+
+        testData.add(new String[]{"!pokemon 900-", PokemonServiceImpl.POKEMON_RETURN_NULL});
+        testData.add(new String[]{"!pokemon 898-", "No. 898 Calyrex"});
+        testData.add(new String[]{"!pokemon 897-", "No. 898 Calyrex", "No. 897 Spectrier"});
+        testData.add(new String[]{"!pokemon 896-", "No. 898 Calyrex", "No. 897 Spectrier", "No. 896 Glastrier"});
+
+        for (String[] data : testData)
+        {
+            Set<String> results = new HashSet<>();
+
+            int expected_results = data.length - 1;
+
+            int test_ten_times_for_each = expected_results * 10;
+            for (int i = 0; i < test_ten_times_for_each; i++)
+            {
+                String text = impl.process(data[0]);
+                results.add(text);
+            }
+
+            assertEquals(expected_results, results.size());
+
+            for (int i = 1; i < data.length; i++)
+            {
+                String result = data[i];
+                assertTrue(results.contains(result), "Doesn't contain " + result);
+            }
+        }
+    }
+
 
     @BeforeEach
     public void setup() throws Exception
