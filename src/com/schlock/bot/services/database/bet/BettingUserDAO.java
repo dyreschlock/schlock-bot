1ab7a6b76f7f134e9ba04bab8951709d43ae60a8
Jim Hendricks <dyreschlock@gmail.com>
2021-02-13 17:46:18 +0900

    missing base class // #11

--- src/com/schlock/bot/services/database/bet/BettingUserDAO.java
+++ src/com/schlock/bot/services/database/bet/BettingUserDAO.java
@@ -1,8 +1,9 @@
 package com.schlock.bot.services.database.bet;
 
 import com.schlock.bot.entities.bet.BettingUser;
+import com.schlock.bot.services.database.BaseDAO;
 
-public interface BettingUserDAO
+public interface BettingUserDAO extends BaseDAO<BettingUser>
 {
     BettingUser getByUsername(String username);
 }
