82f1e8b093a85103c9c85e5a93abfc94508fdc68
Jim Hendricks <dyreschlock@gmail.com>
2021-03-28 18:37:58 +0900

    adding logging into twitch commands to view incoming and outgoing responses // #56

--- src/com/schlock/bot/services/bot/twitch/Commands.java
+++ src/com/schlock/bot/services/bot/twitch/Commands.java
@@ -7,12 +7,16 @@ import com.schlock.bot.services.commands.ListenerResponse;
 import com.schlock.bot.services.commands.ListenerService;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Set;
 
 public class Commands extends ListenerAdapter
 {
+    private static final Logger log = LoggerFactory.getLogger(Commands.class);
+
     private final Set<ListenerService> listeners;
 
     private final DiscordBot discordBot;
@@ -33,11 +37,14 @@ public class Commands extends ListenerAdapter
         String message = event.getMessage().toLowerCase();
         String username = event.getUser().getNick();
 
+        log.info("INCOMING -- [" + username + "] " + message);
+
         if (message.startsWith(Bot.PING))
         {
             event.getChannel().send().message(Bot.PONG);
             discordBot.relayMessage(Bot.PONG);
 
+            log.info("OUT from Ping -- [" + username + "] " + message);
             return;
         }
 
@@ -61,6 +68,7 @@ public class Commands extends ListenerAdapter
 
                 if(service.isTerminateAfterRequest())
                 {
+                    log.info("OUT from Listener -- [" + username + "] " + message);
                     return;
                 }
             }
@@ -73,8 +81,12 @@ public class Commands extends ListenerAdapter
             {
                 String response = commands.get(command);
                 event.getChannel().send().message(response);
+
+                log.info("OUT from Short -- [" + username + "] " + message);
                 return;
             }
         }
+
+        log.info("OUT from Nothing -- [" + username + "] " + message);
     }
 }
