4d356299b99d150755328a7243c5b3c7057460de
Jim Hendricks <dyreschlock@gmail.com>
2023-04-08 14:17:13 +0900

    adding a generated date to the index page // #124

--- src/com/schlock/bot/pages/pokemon/Index.java
+++ src/com/schlock/bot/pages/pokemon/Index.java
@@ -1,5 +1,24 @@
 package com.schlock.bot.pages.pokemon;
 
+import org.apache.tapestry5.ioc.Messages;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Index
 {
+    private static final String UPDATE_MESSAGE = "update";
+
+    @Inject
+    private Messages messages;
+
+    public String getGeneratedMessage()
+    {
+        SimpleDateFormat format = new SimpleDateFormat("MMMMMM d, YYYY");
+
+        String message = messages.format(UPDATE_MESSAGE, format.format(new Date()));
+        return message;
+    }
 }
